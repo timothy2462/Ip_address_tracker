2d8c370a44bff11c0db5226eb25a5c07932fbb18
timothy <timothy.christian10@gmail.com>
2024-07-21 23:25:37 +0100

    removed console data

--- src/components/IPAddress.jsx
+++ src/components/IPAddress.jsx
@@ -1,7 +1,3 @@
-
-
-
-
 import React, { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 import Stats from "./Stats";
@@ -22,11 +18,13 @@ const IPAddress = () => {
 
   const fetchLocation = (ipAddress = "") => {
     fetch(
-      `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress || defaultIPAddress}`
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${
+        ipAddress || defaultIPAddress
+      }`
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         setIPAddress(data.ip);
         setLocation(
           `${data.location.city}, ${data.location.country} ${data.location.postalCode}`
